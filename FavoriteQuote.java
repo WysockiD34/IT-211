e9ea2a406d9cdf956bedbcabdda12ded717f3719
WysockiD34 <114697378+WysockiD34@users.noreply.github.com>
2023-01-20 23:05:24 -0800

    Add files via upload

---
+++ FavoriteQuote.java
@@ -0,0 +1,7 @@
+public class FavoriteQuote {
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        String output = "All those days that came and went, little did I know that they were life.";
+        System.out.println(output);
+    }    
+}
