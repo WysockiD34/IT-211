9b4de8d9922342c3c3075ab52847e458ed3a28f1
WysockiD34 <114697378+WysockiD34@users.noreply.github.com>
2023-01-23 18:55:45 -0800

    Create DollarsToPesos.java

---
+++ DollarsToPesos.java
@@ -0,0 +1,13 @@
+public class dollarsTopeso {
+    public static void main(String[] args) {
+        double dollars;
+        double peso;
+
+        dollars = 1.00;
+    
+        // convert U.S Dollar to Peso (This is the current conversion rate)
+        peso = (dollars * 18.81);
+
+        System.out.print(
+            dollars + " U.S Dollar is equal to " + peso + " Mexican Pesos.");
+    }    
